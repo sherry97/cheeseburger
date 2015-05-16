f310331020c09b6a5a3f54bfaa480d1285c86c57
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-05-16 18:15:12 -0400

    Fix images to png

--- item.py
+++ item.py
@@ -28,25 +28,25 @@ class Item():
 		return self.filename
 
 	def __str__(self):
-		return "<{}: {}; {} cal>".format(category, name, calories)
+		return "<{}: {}; {} cal>".format(self.category, self.name, self.calories)
 
 	def catalogue():
 		items = []
 		# name, category, calories, delishness, filename
 		items.append(Item("tomato", topping, 6, 5, "tomato.png"))
-		items.append(Item("lettuce", topping, 2, 5, "lettuce.jpeg"))
-		items.append(Item("mushrooms", topping, 7, 5, "mushrooms.jpeg"))
-		items.append(Item("pickles", topping, 4, 10, "pickle.jpeg"))
-		items.append(Item("bacon", topping, 10, 5, "bacon.jpeg"))
-		items.append(Item("ketchup", condiment, 10, 5, "ketchup.jpeg"))
-		items.append(Item("mustard", condiment, 10, 5, "mustard.jpeg"))
-		items.append(Item("mayo", condiment, 10, 5, "mayo.jpeg"))
-		items.append(Item("BBQ", condiment, 10, 5, "bbq.jpeg"))
-		items.append(Item("horse radish", condiment, 10, 5, "horse.jpeg"))
-		items.append(Item("beef", meat, 10, 5, "cow.jpeg"))
-		items.append(Item("tofu", meat, 10, 5, "tofu.jpeg"))
-		items.append(Item("fish", meat, 10, 5, "fish.jpeg"))
-		# items.append(Item("portobello mushrooms", meat, 10, 5, "xxx.jpeg"))
+		items.append(Item("lettuce", topping, 2, 5, "lettuce.png"))
+		items.append(Item("mushrooms", topping, 7, 5, "mushrooms.png"))
+		items.append(Item("pickles", topping, 4, 10, "pickle.png"))
+		items.append(Item("bacon", topping, 10, 5, "bacon.png"))
+		items.append(Item("ketchup", condiment, 10, 5, "ketchup.png"))
+		items.append(Item("mustard", condiment, 10, 5, "mustard.png"))
+		items.append(Item("mayo", condiment, 10, 5, "mayo.png"))
+		items.append(Item("BBQ", condiment, 10, 5, "bbq.png"))
+		items.append(Item("horse radish", condiment, 10, 5, "horse.png"))
+		items.append(Item("beef", meat, 10, 5, "cow.png"))
+		items.append(Item("tofu", meat, 10, 5, "tofu.png"))
+		items.append(Item("fish", meat, 10, 5, "fish.png"))
+		# items.append(Item("portobello mushrooms", meat, 10, 5, "xxx.png"))
 		items.append(Item("regular", bun, 10, 5, "regular.png"))
 		items.append(Item("sesame", bun, 10, 5, "sesame.png"))
 		return items
--- main_game.py
+++ main_game.py
@@ -1,7 +1,15 @@
 # HackTJ 2015
 
-
+from tkinter import Tk, Frame, BOTH, PhotoImage, Label
+from random import random
+from item import Item
 
 def main():
-	pass
-if __name__ == '__main__'
+	root = Tk()
+	root.title("Testing images")
+	items = Item.catalogue()
+	photo = PhotoImage(file = items[int(random() * len(items))].filename)
+	label = Label(root, image=photo)
+	label.grid()
+	root.mainloop()
+if __name__ == '__main__': main()
