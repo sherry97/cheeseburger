827c7f0b1aa1433fc9e2c50f3cad3552e4e1aebd
sherry97 <sherry97@gmail.com>
2015-05-16 19:07:51 -0400

    test

--- test.py
+++ test.py
@@ -8,18 +8,21 @@ def setUpCatalogue():
 
 def gatherMaterials(chara, catalogue):
 	for i in range(len(catalogue)):
-		print(catalogue[i])
 		chara.addToBackpack(catalogue[i])
 
 def makeCheeseburger(chara):
 	contents = chara.backpackContents()
+	prevcat = ''
 	for i in range(len(contents)):
 		item = chara.viewItem(i)
+		if not item.category == prevcat: print()
 		print(str(i)+". "+item.__str__())
-	topping =   int(input("enter...topping #: "))
+		prevcat = item.category
+	topping =   int(input("\nenter...topping #: "))
 	condiment = int(input("   ...condiment #: "))
 	meat =      int(input("        ...meat #: "))
 	bun =       int(input("         ...bun #: "))
+	print()
 	ingredients = [contents[topping], contents[condiment], contents[meat], contents[bun]]
 	c = Cheeseburger()
 	for i in ingredients:
