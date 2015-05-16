67e663f1e795b5b9cd23b03b90109eb7c95baefc
sherry97 <sherry97@gmail.com>
2015-05-16 16:55:44 -0400

    added deliciousness value

--- cheeseburger.py
+++ cheeseburger.py
@@ -1,21 +1,34 @@
 class Cheeseburger():
 	calories = 0
+	delish = 0
+	size = 0
 	content = []
 
 	def __init__():
 		self.calories = calories
+		self.delish = delish
+		self.size = size
 		self.content = content
 
 	def calories():
 		return calories
 
+	def delish():
+		return delish
+
 	def add(o):
 		content[-1] = o
 		calories += o.calories()
+		rawdelish = delish*size+o.delish()
+		size += 1
+		delish = rawdelish/size
 
 	def remove(o):
 		content.remove(o);
 		calories -= o.calories()
+		rawdelish = delish*size-o.delish()
+		size -= 1
+		delish = rawdelish/size
 
 	def display():
 		print(content)
--- person.py
+++ person.py
@@ -1,21 +1,29 @@
 class Person():
 	hunger = 50
+	happy = 5
 	backpack = Backpack();
 
 	def __init__():
 		self.hunger = hunger
+		self.happy = happy
 		self.backpack = Backpack();
 
 	def eat(c):
 		hunger += c.calories();
+		happy += c.delish();
 		if hunger > 100: hunger = 100
+		if happy > 10: happy = 10
 		return hunger
 
 	def move():
 		hunger -= 5
+		happy -= .5
 
 	def hunger():
 		return hunger
+
+	def happy():
+		return happy
 
 	def sethunger(val):
 		hunger = val
