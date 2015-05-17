7960d3a0eaf27fec9b9641de0cacd8dfafae2223
Hyungju Moon <2016hmoon@tjhsst.edu>
2015-05-16 21:41:29 -0400

    added maze, need to add food now

--- main_game.py
+++ main_game.py
@@ -13,6 +13,37 @@ color_coding = {"meat": "#993300", "topping": "#99FF66", "condiment": "#FFCC00",
 # 	canvas = Canvas(root, width  = WIDTH, height = HEIGHT) #, bg = 'black')
 # 	canvas.pack() #expand = YES, fill = BOTH)
 # 	return canvas
+from random import shuffle, randrange
+
+def make_maze(w = 10, h = 10):
+	vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
+	ver = [["| "] * w + ['|'] for _ in range(h)] + [[]]
+	hor = [["+-"] * w + ['+'] for _ in range(h + 1)]
+	def walk(x, y):
+		vis[y][x] = 1
+		d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+		shuffle(d)
+		for (xx, yy) in d:
+			if vis[yy][xx]: continue
+			if xx == x: hor[max(y, yy)][x] = "+ "
+			if yy == y: ver[y][max(x, xx)] = "  "
+			walk(xx, yy)
+	l=[]
+	walk(randrange(w), randrange(h))
+	for (a, b) in zip(hor, ver):
+		l.append(''.join(a))
+		l.append(''.join(b))
+	m=[]
+	for a in range(len(l)):
+		n=[]
+		s=l[a]
+		for b in range(len(s)):
+			if s[b]==" ":
+				n.append(0)
+			else:
+				n.append(1)
+		m.append(n)
+	return m
 
 def display_item(r, c, WIDTH, HEIGHT, items, canvas):
 	print("howdy")
@@ -20,13 +51,15 @@ def display_item(r, c, WIDTH, HEIGHT, items, canvas):
 	idd = canvas.create_image(WIDTH / 3 * r, HEIGHT / 3 * c, anchor=NW, image=img)
 
 def main():
-	M_WIDTH = 50 ; M_HEIGHT = 35
+	M_WIDTH = 51 ; M_HEIGHT = 35	
 	WIDTH = M_WIDTH * 20 ; HEIGHT = M_HEIGHT * 20
 
 	master = Tk()
 
 	canvas = Canvas(master, width=WIDTH, height=HEIGHT)
 	canvas.pack()
+
+	maze=make_maze(int(M_HEIGHT*0.5), int(M_WIDTH*0.5-0.5))
 
 	items = Item.catalogue()
 
@@ -35,12 +68,16 @@ def main():
 			# display_item(r, c, WIDTH, HEIGHT, items, canvas)
 			# img = PhotoImage(file=items[r * 3 + c].filename)
 			# idd = canvas.create_image(WIDTH / 3 * r, HEIGHT / 3 * c, anchor=NW, image=img)
-			item = items[int(random() * len(items))]
-			item_color = color_coding[item.category]
+			#item = items[int(random() * len(items))]
+
+			#item = items[int(random() * len(items))]
+			item_color = ("#000000" if maze[x][y]==0 else "#FFFFFF")
+			#item_color = color_coding[item.category]
 			canvas.create_rectangle(WIDTH / M_WIDTH * x, HEIGHT / M_HEIGHT * y, 
 				WIDTH / M_WIDTH * (x + 1), HEIGHT / M_HEIGHT * (y + 1), fill = item_color)
 
 			# canvas.pack()
 
 	master.mainloop()
+
 if __name__ == '__main__': main()
