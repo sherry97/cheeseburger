1b8905a26e9612156b8c03d1dccbc6c5ec48cab5
Kevin Chaplin <kevinchaplin58@gmail.com>
2015-05-16 21:22:29 -0400

    Save Kevin's stuff?

--- main_game.py
+++ main_game.py
@@ -4,6 +4,7 @@ from tkinter import *
 from random import random
 from item import Item
 from time import time
+from maze import Maze
 
 color_coding = {"meat": "#993300", "topping": "#99FF66", "condiment": "#FFCC00",
 	"bun": "#996600", "cheese": "#FFFF66", "blank": "#000000", "wall": "#FFFFFF"}
@@ -20,12 +21,19 @@ def display_item(r, c, WIDTH, HEIGHT, items, canvas):
 	idd = canvas.create_image(WIDTH / 3 * r, HEIGHT / 3 * c, anchor=NW, image=img)
 
 def main():
-	M_WIDTH = 50 ; M_HEIGHT = 35
-	WIDTH = M_WIDTH * 20 ; HEIGHT = M_HEIGHT * 20
+	M_WIDTH = 51 ; M_HEIGHT = 35
+	G_WIDTH = M_WIDTH * 20 ; G_HEIGHT = M_HEIGHT * 20
+	T_WIDTH = G_WIDTH + 200 ; T_HEIGHT = G_HEIGHT
+	# maze, game, total
+
+	maz = Maze(M_WIDTH, M_HEIGHT).getMaze()
+	# maze = [[int(random() * 1.2) for c in range(M_WIDTH)] for r in range(M_HEIGHT)]
+
+
 
 	master = Tk()
 
-	canvas = Canvas(master, width=WIDTH, height=HEIGHT)
+	canvas = Canvas(master, width=T_WIDTH, height=T_HEIGHT)
 	canvas.pack()
 
 	items = Item.catalogue()
@@ -35,10 +43,16 @@ def main():
 			# display_item(r, c, WIDTH, HEIGHT, items, canvas)
 			# img = PhotoImage(file=items[r * 3 + c].filename)
 			# idd = canvas.create_image(WIDTH / 3 * r, HEIGHT / 3 * c, anchor=NW, image=img)
-			item = items[int(random() * len(items))]
-			item_color = color_coding[item.category]
-			canvas.create_rectangle(WIDTH / M_WIDTH * x, HEIGHT / M_HEIGHT * y, 
-				WIDTH / M_WIDTH * (x + 1), HEIGHT / M_HEIGHT * (y + 1), fill = item_color)
+			item_color = color_coding["blank"]
+			if maz[x][y] == 1: #wall
+				item_color = color_coding["wall"]
+			elif random() < 0.1:
+				item_index = int(random() * len(items))
+				item = items[item_index]
+				item_color = color_coding[item.category]
+				maz[x][y] = item_index + 2
+			canvas.create_rectangle(G_WIDTH / M_WIDTH * x, G_HEIGHT / M_HEIGHT * y, 
+				G_WIDTH / M_WIDTH * (x + 1), G_HEIGHT / M_HEIGHT * (y + 1), fill = item_color)
 
 			# canvas.pack()
 
